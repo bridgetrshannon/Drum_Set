8084cbe775444d0cf32064d0d20fbd2ded6c98cc
Bridget Shannon <bridgetrshannon@gmail.com>
2021-01-25 16:03:12 -0500

    keys div now centered on page

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 window.addEventListener("keydown", function (e) {
   // searching for audio attribute, using template literal to get the data
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}]`);
+
+  //   stops function from running if no corresponding key is pressed
+  if (!audio) return;
+
+  //   rewinds audio to the start so it will play repeatedly rather than running through the entire clip first
+  audio.currentTime = 0;
+
+  //   plays audio
+  audio.play();
+
+  console.log(key);
 });
